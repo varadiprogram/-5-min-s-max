9fcae6887804421ae1051cb4c6e3fe358c6cd279
im_varadi <97060168+varadiprogram@users.noreply.github.com>
2024-01-12 21:45:43 +0100

    Add files via upload

---
+++ palfi_kerdesek.py
@@ -0,0 +1,19 @@
+szamok = []
+vege = 5
+paros = 0
+
+while vege != 0:
+    szam = int(input("Szám: "))
+
+    if szam % 2 == 0:
+        paros = paros + 1
+
+    szamok.append(szam)
+    vege = vege - 1
+
+legnagyobb_szam = max(szamok)
+legkisebb_szam = min(szamok)
+
+print(f"Ennyi szám osztható kettővel: {paros}")
+print(
+    f"legnagyobb szám: {legnagyobb_szam}\nLegkissebb szám: {legkisebb_szam}")
